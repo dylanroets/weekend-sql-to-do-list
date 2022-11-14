2be404c1862aff3f5e6f9bbe067b23c42db78b58
Dylan Roets <dylanroets@yahoo.com>
2022-11-14 16:28:23 -0600

    added fixed bug

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -9,7 +9,7 @@ if(process.env.DATABASE_URL) {
         }
     });
 } else {
-    pool = new Pool({
+    pool = new pg.Pool({
         database: 'weekend-to-do-app',
         host: 'localhost',
         port: 5432,
