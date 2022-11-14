65e5f6c6757f9919d9f9da34b92e24a76cbef606
Dylan Roets <dylanroets@yahoo.com>
2022-11-14 16:06:30 -0600

    added heroku compat

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,23 @@
 const pg = require('pg');
 const Pool = pg.Pool;
-const pool = new Pool({
-    database: 'weekend-to-do-app',
-    host: 'localhost',
-    port: 5432,
-    max: 10,
-    idleTimeoutMillis: 30000
-});
+let pool;
+if(process.env.DATABASE_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+} else {
+    pool = new Pool({
+        database: 'weekend-to-do-app',
+        host: 'localhost',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000
+    });
+}
+
 
 // quick console.log on connection
 pool.on('connect', ()=>{
